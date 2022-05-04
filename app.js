4ee5a1577b305580b6cd76035b94462540bd0a3d
Tito23 <jorge591vl@gmail.com>
2022-05-04 15:00:44 -0500

    update: creamos el boton comprar y x para que agregue compras y elimine el producto.T23

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ function elementSelected(event) {
       nameProduct = element.name
       imgProduct = element.img
       priceProduct = element.price
-      
     }
   })
   createCard(nameProduct, imgProduct, priceProduct)
@@ -31,16 +30,36 @@ function createCard(nameProduct, imgProduct, priceProduct) {
   const imgCard = document.createElement('img');
   const title = document.createElement('h3');
   const price = document.createElement("p");
+  const button = document.createElement('button');
+  const button2 = document.createElement('button');
   imgCard.setAttribute('src',imgProduct);
   imgCard.setAttribute('alt',nameProduct);
   title.textContent = nameProduct;
   price.textContent = priceProduct;
+  button.textContent = 'Comprar'
+  button2.textContent = 'X'
   card.appendChild(imgCard);
   card.appendChild(title);
   card.appendChild(price);
   main.appendChild(card);
+  card.appendChild(button);
+  card.appendChild(button2);
+  button.classList.add("btn-add")
+  button2.classList.add("btn-add")
+  imgCard.classList.add("img-card")
   card.classList.add("card");
 //   // card.classList.remove("cardPrueba")
+
+
+    button.addEventListener('click', compra)
+    button2.addEventListener('click', quitar)
+
+    function compra(){
+      alert(`se ha comprado el ${title.textContent}`)
+    }
+    function quitar(){
+      main.removeChild(card)
+    }
 }
 
 
